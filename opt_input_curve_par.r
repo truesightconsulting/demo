18cbe0371df32db67e6bc79974c695124b3d6e88
Xin Zhou <xinzhou1023@gmail.com>
2016-05-09 17:34:59 -0400

    resturcture

--- opt_input_curve_par.r
+++ opt_input_curve_par.r
@@ -1,45 +1,14 @@
-# prepare curve parameters for optm
-print("Note: Calculating Curve Parameters")
-
-####################################################################################
-# Customized part:
-####################################################################################
-# merge clv table
-curve=merge(curve,ex.clv[,c("sales1_id","clv"),with=F],by=c("sales1_id"),all.x=T)
-
-
-# set a based on clv value
-curve$a.decomp=curve$a
-if(ex.setup$optimization_target==2){
-  curve=curve[,a:=a*clv]
+# set beta based on clv value
+if (curve_f=="ninah") {
+  curve$beta.decomp=curve$beta
+  if(ex.setup$optimization_target==2){
+    curve=curve[,beta:=beta*clv]
+  }else if (ex.setup$optimization_target==3){
+    curve=curve[,beta:=beta/factor_1]
+  }
+}else if (curve_f %in% c("exp","exp000")){
+  curve$a.decomp=curve$a
+  if(ex.setup$optimization_target==2){
+    curve=curve[,a:=a*clv]
+  }
 }
-
-# backup curve table
-curve.org=curve
-
-# response curve function
-calc_decomp=function(x){
-  # x is spend; output is sales
-  curve$a.decomp*(1-exp(-curve$b*x/curve$cps))
-}
-
-calc_npv=function(x){
-  # x is spend; output is sales
-  curve$a*(1-exp(-curve$b*x/curve$cps))
-}
-
-# # decomp function
-# calc_decomp=function(x){
-#   # x is spend; output is sales
-#   ad=function(x1,x2){(1 - ((1 - x1 * exp(1) ^ (log(0.5) / curve$hl)) / (exp(1) ^ ((x2/curve$cps) / 
-#                                                                                     (curve$wks * curve$max) * (-log(1 - curve$hrf)) * 10))))}
-#   curve$beta2*ad(x1=ad(x1=rep(0,length(curve$wks)),x2=x),x2=x)
-# }
-# 
-# # npv function
-# calc_npv=function(x){
-#   # x is spend; output is sales
-#   ad=function(x1,x2){(1 - ((1 - x1 * exp(1) ^ (log(0.5) / curve$hl)) / (exp(1) ^ ((x2/curve$cps) / 
-#                                                                                     (curve$wks * curve$max) * (-log(1 - curve$hrf)) * 10))))}
-#   curve$beta1*ad(x1=ad(x1=rep(0,length(curve$wks)),x2=x),x2=x)
-# }
